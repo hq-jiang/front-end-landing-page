fd04fb3d00421196361e50975532c89b71155add
Hanqiu Jiang <hanq.jiang@gmail.com>
2020-03-29 21:12:51 +0200

    feat: Add styled navbar elements with javascript

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
-### Landing Page
-
 This code is part of the Udacity front-end web developer nano degree and was originally taken from:
 https://github.com/udacity/fend.git
 
+
+### Landing Page
 This project demonstrates the use of Javascript to dynamically add elements to the website.
--- js/app.js
+++ js/app.js
@@ -1,37 +1,49 @@
 /**
- * 
+ *
  * Manipulating the DOM exercise.
  * Exercise programmatically builds navigation,
  * scrolls to anchors from navigation,
  * and highlights section in viewport upon scrolling.
- * 
+ *
  * Dependencies: None
- * 
+ *
  * JS Version: ES2015/ES6
- * 
+ *
  * JS Standard: ESlint
- * 
+ *
 */
 
 /**
  * Define Global Variables
- * 
+ *
 */
 
 
 /**
  * End Global Variables
  * Start Helper Functions
- * 
+ *
 */
+function print() {
+  console.log("Hello World");
+}
 
-
-
+function AddElementToNavbar() {
+  const navbar = document.querySelector("#navbar__list");
+  let listElement = document.createElement("li")
+  listElement.textContent = "More";
+  listElement.classList.add("navbar__elem");
+  navbar.appendChild(listElement);
+  console.log("Adding to Navbar")
+}
 /**
  * End Helper Functions
  * Begin Main Functions
- * 
+ *
 */
+
+let body = document.querySelector("body");
+body.addEventListener("click", AddElementToNavbar);
 
 // build the nav
 
@@ -45,13 +57,11 @@
 /**
  * End Main Functions
  * Begin Events
- * 
+ *
 */
 
-// Build menu 
+// Build menu
 
 // Scroll to section on link click
 
 // Set sections as active
-
-
